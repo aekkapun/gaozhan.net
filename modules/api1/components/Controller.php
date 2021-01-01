9dd46a2d40193fa99edc0a74eba2c21d7e58865b
ezsky <ezskyyoung@gmail.com>
2021-01-01 15:16:59 +0800

    feat: api增加http base auth 认证方式

--- modules/api1/components/Controller.php
+++ modules/api1/components/Controller.php
@@ -5,9 +5,36 @@ namespace app\modules\api1\components;
 
 
 use app\modules\api1\models\User;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBasicAuth;
 
 class Controller extends \yii\rest\Controller
 {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => CompositeAuth::className(),
+            'authMethods' => [
+                'basicAuth' => [
+                    'class' => HttpBasicAuth::className(),
+                    'auth' => function ($username, $password) {
+                        $user = User::find()->where(['username' => $username])->one();
+                        if ($user !== null && $user->validatePassword($password)) {
+                            return $user;
+                        }
+                        return null;
+                    },
+                ],
+            ],
+        ];
+        return $behaviors;
+    }
+
     /**
      * Returns current user.
      * Should be used in order to support token-less auth via web session that is handy for AJAX.
